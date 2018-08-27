562fe33f0b5d321ba25622f7cc29488e71536a19
GogoWwz <wwz_zjut_web@163.com>
2018-08-27 23:43:17 +0800

    利用webpack4.x搭建开发环境

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+// 创建插件示例对象
+const htmlPlugin = new HtmlWebpackPlugin({
+    // 模板文件
+    template: path.join(__dirname, './src/index.html'),
+    //目标文件
+    filename: "index.html"
+})
+
+// 向外暴露一个一个webpack配置文件
+// 用module.export 的原因是因为 webpack 是基于node构建的
+// export 是es6 的关键字，并不是node的，所以用不了
+module.exports = {
+    // 设置mode：是生产环境还是开发环境
+    mode: "development",
+    // 在webpack4.x中新增特性： 约定大于配置；约定默认的打包路径是 src -> index.js
+    plugins: [
+        htmlPlugin
+    ]
+}
